3c36a4d5321e1ab0df37fecd2edafe71154871e4
SvirskyiAndrii <a.svirskyi@ghostdrive.com>
2022-12-07 00:19:18 +0200

    Fixed onClick function

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -15,30 +15,60 @@ export const Main = ({ children }: MainLayoutProps) => {
     const [data, setData] = useState<any>(null);
     const [modalType, setModalType] = useState<string | null>(null);
 
+    const getData = (e: any) => {
+        const team = e.target.getAttribute('data-team');
+        const top = e.clientY;
+        const left = e.clientX;
+        if (team) {
+            const github = e.target.getAttribute('data-github');
+            const documentation = e.target.getAttribute('data-documentation');
+            setData({ team, 
+                github, 
+                documentation, 
+                top, 
+                left });
+        } else {
+            const closest = e.target.closest('[data-team]');
+            if (closest) {
+                const team = closest.getAttribute('data-team');
+                const github = closest.getAttribute('data-github');
+                const documentation = closest.getAttribute('data-documentation');
+                setData({ team, 
+                    github, 
+                    documentation, 
+                    top, 
+                    left });
+            } else {
+                setData(null);
+            }
+        }
+    };
+
     return (
-        <div
-            className={styles.main}
-            onClick={(e: any) => {
-                const team = e.target.getAttribute('data-team');
-                if (team) {
-                    const github = e.target.getAttribute('data-github');
-                    const documentation = e.target.getAttribute('data-documentation');
-                    const top = e.clientY;
-                    const left = e.clientX;
-                    setData({ team, 
-                        github, 
-                        documentation, 
-                        top, 
-                        left });
-                } else {
-                    setData(null);
-                }
-            }}
-        >
-            <Header setModalType={setModalType} />
-            <div className={styles.wrapper}>
-                <Sidebar />
-                <div className={styles.content}>{children}</div>
+        <>
+            <div
+                className={styles.main}
+                onClick={getData}
+            >
+                <Header setModalType={setModalType} />
+                <div className={styles.wrapper}>
+                    <Sidebar />
+                    <div className={styles.content}>{children}</div>
+                </div>
+                {modalType && (
+                    <Modal
+                        team="Second"
+                        github="github"
+                        documentation="documentation"
+                        close={() => {
+                            setModalType(null);
+                        }}
+                    >
+                        {modalType === 'sign-in' 
+                            ? <SignIn /> 
+                            : <SignUp />}
+                    </Modal>
+                )}
             </div>
             {data && (
                 <Dropdown
@@ -49,20 +79,6 @@ export const Main = ({ children }: MainLayoutProps) => {
                     left={data?.left}
                 />
             )}
-            {modalType && (
-                <Modal
-                    team="Second"
-                    github="github"
-                    documentation="documentation"
-                    close={() => {
-                        setModalType(null);
-                    }}
-                >
-                    {modalType === 'sign-in' 
-                        ? <SignIn /> 
-                        : <SignUp />}
-                </Modal>
-            )}
-        </div>
+        </>
     );
 };
